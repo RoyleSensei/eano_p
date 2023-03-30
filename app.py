55208ba7c460f7a8ba7403ef3205ba704a8047bc
Zifan_Zhang <zifan.ivan.zhang@gmail.com>
2023-03-30 19:43:20 -0400

    add: flaks app secret key update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ users = {}
 app = Flask(__name__)
 app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
 app.config["SECRET_KEY"] = os.environ.get(
-    "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
+    "SECRET_KEY", secrets.token_urlsafe(16)
 )
 client = MongoClient(os.getenv("MONGODB_URI"))
 app.db = client.eano_community
